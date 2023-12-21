8050f2f303db31dfea7d4f46e2da02cab87d5166
sergeyzoloto <sergiozoloto@gmail.com>
2023-12-21 13:27:08 +0100

    login end point created

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-import express, { json } from 'express';
+import express, { json, request, response } from 'express';
 import cors from 'cors';
 import { connect } from 'mongoose';
 import User from './models/User.js';
+
 import { config } from 'dotenv';
+config();
 
 import bcryptjs from 'bcryptjs';
 const salt = bcryptjs.genSaltSync();
-
-config();
+import jwt from 'jsonwebtoken';
+const secret = process.env.JWT_SECRET;
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 app.use(json());
 
 // The environment should set the port
@@ -28,6 +30,21 @@ app.post('/register', async (request, response) => {
     response.json(userDoc);
   } catch (error) {
     response.status(400).json(error);
+  }
+});
+
+app.post('/login', async (request, response) => {
+  const { username, password } = request.body;
+  const userDoc = await User.findOne({ username });
+  const passwordCheck = bcryptjs.compareSync(password, userDoc.password);
+  if (passwordCheck) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (error, token) => {
+      if (error) throw error;
+      response.cookie('token', token).json('ok');
+    });
+  } else {
+    response.status(400).json('wrong credentials');
   }
 });
 
